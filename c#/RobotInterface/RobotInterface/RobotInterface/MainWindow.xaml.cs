8894882bb7a8908809d282ed9d31044080d54b19
IUT GEII Table 5 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2024-10-15 17:57:16 +0200

    interface

---
+++ c#/RobotInterface/RobotInterface/RobotInterface/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using ExtendedSerialPort_NS;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RobotInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ExtendedSerialPort serialPort1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        bool toggle = false;
+        private void SendMessage()
+        {
+            string value = textboxEmission.Text;            
+            textboxReception.Text += "Recu : " + value + "\n";
+            textboxEmission.Text = "";
+        }
+
+        private void buttonEnvoyer_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (toggle == true)
+            {
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+                toggle = false;
+            }
+            else
+            {
+                buttonEnvoyer.Background = Brushes.Beige;
+                toggle = true;
+            }
+            textboxReception.Text += textboxEmission.Text + "\n";
+            
+
+            textboxEmission.Text = "";
+
+        }
+
+        private void textboxEmission_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+
+        }
+    }
+}
