a2d057f8a4b889592caae3dd564847f84243ee42
IUT GEII Table 5 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2024-10-22 16:28:17 +0200

    interfazce

--- c#/RobotInterface/RobotInterface/RobotInterface/MainWindow.xaml.cs
+++ c#/RobotInterface/RobotInterface/RobotInterface/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace RobotInterface
 {
@@ -19,17 +20,25 @@ namespace RobotInterface
     public partial class MainWindow : Window
     {
         ExtendedSerialPort serialPort1;
-
         public MainWindow()
         {
+            serialPort1 = new ExtendedSerialPort("COM14", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+
+            serialPort1.Open();
+
             InitializeComponent();
         }
         bool toggle = false;
         private void SendMessage()
         {
-            string value = textboxEmission.Text;            
-            textboxReception.Text += "Recu : " + value + "\n";
+            string value = textboxEmission.Text;
+            serialPort1.WriteLine(value);
             textboxEmission.Text = "";
+        }
+        public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
         }
 
         private void buttonEnvoyer_Click_1(object sender, RoutedEventArgs e)
@@ -44,11 +53,7 @@ namespace RobotInterface
                 buttonEnvoyer.Background = Brushes.Beige;
                 toggle = true;
             }
-            textboxReception.Text += textboxEmission.Text + "\n";
-            
-
-            textboxEmission.Text = "";
-
+            SendMessage();
         }
 
         private void textboxEmission_KeyUp_1(object sender, KeyEventArgs e)
