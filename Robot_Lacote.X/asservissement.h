764e4bef41f3cee0a77025b814924eb8ddb8133a
iutgeiiE105 <touloniut@gmail.com>
2025-05-06 14:30:03 +0200

    ggt

--- Robot_Lacote.X/asservissement.h
+++ Robot_Lacote.X/asservissement.h
@@ -1,23 +1,37 @@
-/* 
- * File:   asservissement.h
- * Author: E306-PC5
- *
- * Created on 29 avril 2025, 13:59
- */
-
 #ifndef ASSERVISSEMENT_H
-#define	ASSERVISSEMENT_H
+#define ASSERVISSEMENT_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+#define FREQ_ECH_QEI 100.0 // Fréquence d'échantillonnage en Hz
 
+typedef struct _PidCorrector
+{
+    double Kp;
+    double Ki;
+    double Kd;
+    double erreurProportionelleMax;
+    double erreurIntegraleMax;
+    double erreurDeriveeMax;
+    double erreurIntegrale;
+    double epsilon_1;
+    double erreur;
+    // For Debug only
+    double corrP;
+    double corrI;
+    double corrD;
+} PidCorrector;
 
+// Fonction pour limiter une valeur dans un intervalle
+double LimitToInterval(double value, double min, double max);
 
+// Fonction de configuration du PID
+void SetupPidAsservissement(volatile PidCorrector* PidCorr, double Kp, double Ki, double Kd, 
+                           double proportionelleMax, double integralMax, double deriveeMax);
 
-#ifdef	__cplusplus
-}
-#endif
+// Fonction du correcteur PID
+double Correcteur(volatile PidCorrector* PidCorr, double erreur);
 
-#endif	/* ASSERVISSEMENT_H */
+// Fonction de mise à jour de l'asservissement
+void UpdateAsservissement();
+
+#endif // ASSERVISSEMENT_H
 
